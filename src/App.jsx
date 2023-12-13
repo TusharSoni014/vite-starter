ad8ea4f7ca42b2cd82bb051533dda58117c43184
Tushar Soni <71227235+TusharSoni014@users.noreply.github.com>
2023-12-13 14:51:47 +0530

    added react.js with vite, redux toolkit, react router dom and tailwind

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Quibusdam maiores, neque dolor alias facilis ipsum repudiandae
+              quam eaque vitae, atque doloribus culpa nisi provident. Dolorum
+              excepturi cumque reiciendis exercitationem quos.
+            </>
+          }
+        ></Route>
+      </Routes>
+    </>
+  );
+}
+
+export default App;
